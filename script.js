d1a91a911873e5db9ed0599e21b9f1451b5a8097
Teiko <teikomancini1993@gmail.com>
2020-10-20 20:26:31 +0200

    boh

--- script.js
+++ script.js
@@ -6,12 +6,17 @@ function multiply(a, b) {
     return a + b;
   };
   
-  const calculator4 = (callback, a, b) => {
+  const calculator3 = (callback, a, b) => {
     console.log(
-      `The result is ${callback == sum ? (a+b) : (a*b)} Teiko refractoring`
+      `The result is ${callback == sum ? (a+b) : (a*b)} :D`
     )
   };
+calculator3(sum, a, b);
+calculator3(multiply, a, b);
+
+a.getElementById("calculator-a");
+b.getElementById("calculator-b")
   
   /* remove this part when .getelementbyidis working
-  calculator4(sum, 92, 27);
-  calculator4(multiply, 56, 44);*/
+  calculator4(sum, a, b);
+  calculator4(multiply, a, b);*/
