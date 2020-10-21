cba75d3364ba78b4bd3e81a82e71e24178313268
Carlos Orellana <carlosaore@gmail.com>
2020-10-21 03:07:37 -0600

    Added divide and substraction, fixed a small error on multiplication

--- script.js
+++ script.js
@@ -4,13 +4,26 @@ function sum() {
   document.getElementById("result").innerHTML = Number(numberA) + Number(numberB);
 }
 
+function substraction() {
+  const numberA = document.getElementById("numberA").value;
+  const numberB = document.getElementById("numberB").value;
+  document.getElementById("result").innerHTML = Number(numberA) - Number(numberB);  
+}
+
 function product() {
+  console.log(document.getElementById("numberA").value === "")
+  const numberA = document.getElementById("numberA").value;
+  const numberB = document.getElementById("numberB").value;
+  document.getElementById("result").innerHTML = Number(numberA) * Number(numberB); 
+}
+
+function divide() {
   console.log(document.getElementById("numberA").value === "")
   const numberA = document.getElementById("numberA").value;
   const numberB = document.getElementById("numberB").value;
   if (numberA == "" || numberB == "" || numberB == 0 || numberA == 0) {
     document.getElementById("result").innerHTML = "black hole"
   } else {
-    document.getElementById("result").innerHTML = Number(numberA) + Number(numberB);
+    document.getElementById("result").innerHTML = Number(numberA) / Number(numberB);
   }  
 }
